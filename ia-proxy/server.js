fdd107eec6e6f52f82a3e2b1459d94524b71a81e
bhdspro <bhdspro@gmail.com>
2025-10-26 11:01:58 -0300

    Update server.js

--- ia-proxy/server.js
+++ ia-proxy/server.js
@@ -7,7 +7,9 @@ const app = express();
 // ==========================
 const ALLOWED_ORIGINS = new Set([
   "https://apps.grupobhds.com",
-  "https://grupobhds.com"
+  "https://grupobhds.com",
+  "http://localhost:3000",
+  "http://127.0.0.1:5500"
 ]);
 
 function isOriginAllowed(origin) {
